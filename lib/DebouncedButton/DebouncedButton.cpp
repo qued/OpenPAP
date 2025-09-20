9265c2a719fbe87cf4235b1f23baed01ffe83dc9
Qued <alan.c.bertl@gmail.com>
2025-09-20 14:06:47 -0500

    debounced button bug fix

--- lib/DebouncedButton/DebouncedButton.cpp
+++ lib/DebouncedButton/DebouncedButton.cpp
@@ -1,32 +1,32 @@
 #include "DebouncedButton.h"
 
-DebouncedButton::DebouncedButton(uint8_t pin, bool pullup, unsigned long debounceMs)
-  : _pin(pin), _pullup(pullup), _debounceDelay(debounceMs) {
-  pinMode(_pin, pullup ? INPUT_PULLUP : INPUT);
-  _currentState = digitalRead(_pin) == (_pullup ? LOW : HIGH);
-  _lastStableState = _currentState;
-  _lastChangeTime = millis();
+DebouncedButton::DebouncedButton(const uint8_t pin, bool pullup, const unsigned long debounceMs)
+    : _pin(pin), _pullup(pullup), _debounceDelay(debounceMs) {
+    pinMode(_pin, pullup ? INPUT_PULLUP : INPUT);
+    _currentState = digitalRead(_pin) == (_pullup ? LOW : HIGH);
+    _lastStableState = _currentState;
+    _lastChangeTime = millis();
 }
 
 void DebouncedButton::update() {
-  bool rawState = digitalRead(_pin) == (_pullup ? LOW : HIGH);
-
-  if (rawState != _lastStableState) {
+    bool rawState = digitalRead(_pin) == (_pullup ? LOW : HIGH);
     unsigned long now = millis();
-    if ((now - _lastChangeTime) > _debounceDelay) {
-      _lastStableState = rawState;
 
-      if (_lastStableState) {
-        _pressedFlag = true;
-      } else {
-        _releasedFlag = true;
-      }
+    if (rawState != _currentState) {
+        _lastChangeTime = now;
+        _currentState = rawState;
     }
-  } else {
-    _lastChangeTime = millis();  // reset timer if still stable
-  }
 
-  _currentState = _lastStableState;
+    if ((now - _lastChangeTime) > _debounceDelay) {
+        if (_currentState != _lastStableState) {
+            _lastStableState = _currentState;
+            if (_lastStableState) {
+                _pressedFlag = true;
+            } else {
+                _releasedFlag = true;
+            }
+        }
+    }
 }
 
 bool DebouncedButton::isPressed() const {
